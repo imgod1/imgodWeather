c9755821b94d15ca4655bb854085d03f7baba2ca
imgod <970720947@qq.com>
2015-03-11 00:01:50 +0800

    imgod first commit

---
+++ src/com/imgodweather/app/Main.java
@@ -0,0 +1,13 @@
+package com.imgodweather.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
